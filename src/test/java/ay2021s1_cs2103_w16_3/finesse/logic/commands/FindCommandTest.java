aec709adeb969c9a273fea00123e7742285b7078
Ian Yong <ianyongyc@gmail.com>
2020-10-16 22:56:50 +0800

    Add FindExpenseCommandTest
    To achieve 100% test coverage for FindExpenseCommand

--- src/test/java/ay2021s1_cs2103_w16_3/finesse/logic/commands/FindCommandTest.java
+++ src/test/java/ay2021s1_cs2103_w16_3/finesse/logic/commands/FindCommandTest.java
@@ -55,7 +55,7 @@ public class FindCommandTest {
     }
 
     @Test
-    public void execute_zeroKeywords_noTransactionFound() {
+    public void execute_zeroKeywords_noTransactionsFound() {
         String expectedMessage = String.format(MESSAGE_TRANSACTIONS_LISTED_OVERVIEW, 0);
         TitleContainsKeywordsPredicate predicate = preparePredicate(" ");
         FindCommand command = new FindCommand(predicate);
--- src/test/java/ay2021s1_cs2103_w16_3/finesse/logic/commands/FindExpenseCommandTest.java
+++ src/test/java/ay2021s1_cs2103_w16_3/finesse/logic/commands/FindExpenseCommandTest.java
@@ -1,6 +1,6 @@
 package ay2021s1_cs2103_w16_3.finesse.logic.commands;
 
-import static ay2021s1_cs2103_w16_3.finesse.commons.core.Messages.MESSAGE_TRANSACTIONS_LISTED_OVERVIEW;
+import static ay2021s1_cs2103_w16_3.finesse.commons.core.Messages.MESSAGE_EXPENSES_LISTED_OVERVIEW;
 import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static ay2021s1_cs2103_w16_3.finesse.testutil.TypicalTransactions.CARL;
 import static ay2021s1_cs2103_w16_3.finesse.testutil.TypicalTransactions.ELLE;
@@ -22,9 +22,9 @@ import ay2021s1_cs2103_w16_3.finesse.model.UserPrefs;
 import ay2021s1_cs2103_w16_3.finesse.model.transaction.TitleContainsKeywordsPredicate;
 
 /**
- * Contains integration tests (interaction with the Model) for {@code FindTransactionCommand}.
+ * Contains integration tests (interaction with the Model) for {@code FindExpenseCommand}.
  */
-public class FindTransactionCommandTest {
+public class FindExpenseCommandTest {
     private Model model = new ModelManager(getTypicalFinanceTracker(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalFinanceTracker(), new UserPrefs());
 
@@ -36,48 +36,48 @@ public class FindTransactionCommandTest {
                 new TitleContainsKeywordsPredicate(Collections.singletonList("second"));
 
         FindCommandStub firstSuperCommand = new FindCommandStub(firstPredicate);
-        FindTransactionCommand firstFindTransactionCommand = new FindTransactionCommand(firstSuperCommand);
+        FindExpenseCommand firstFindExpenseCommand = new FindExpenseCommand(firstSuperCommand);
         FindCommandStub secondSuperCommand = new FindCommandStub(secondPredicate);
-        FindTransactionCommand secondFindTransactionCommand = new FindTransactionCommand(secondSuperCommand);
+        FindExpenseCommand secondFindExpenseCommand = new FindExpenseCommand(secondSuperCommand);
 
         // same object -> returns true
-        assertTrue(firstFindTransactionCommand.equals(firstFindTransactionCommand));
+        assertTrue(firstFindExpenseCommand.equals(firstFindExpenseCommand));
 
         // same values -> returns true
         FindCommandStub firstSuperCommandCopy = new FindCommandStub(firstPredicate);
-        FindTransactionCommand firstFindTransactionCommandCopy = new FindTransactionCommand(firstSuperCommandCopy);
-        assertTrue(firstFindTransactionCommand.equals(firstFindTransactionCommandCopy));
+        FindExpenseCommand firstFindExpenseCommandCopy = new FindExpenseCommand(firstSuperCommandCopy);
+        assertTrue(firstFindExpenseCommand.equals(firstFindExpenseCommandCopy));
 
         // different types -> returns false
-        assertFalse(firstFindTransactionCommand.equals(1));
+        assertFalse(firstFindExpenseCommand.equals(1));
 
         // null -> returns false
-        assertFalse(firstFindTransactionCommand.equals(null));
+        assertFalse(firstFindExpenseCommand.equals(null));
 
         // different predicates -> returns false
-        assertFalse(firstFindTransactionCommand.equals(secondFindTransactionCommand));
+        assertFalse(firstFindExpenseCommand.equals(secondFindExpenseCommand));
     }
 
     @Test
-    public void execute_zeroKeywords_noTransactionsFound() {
-        String expectedMessage = String.format(MESSAGE_TRANSACTIONS_LISTED_OVERVIEW, 0);
+    public void execute_zeroKeywords_noExpensesFound() {
+        String expectedMessage = String.format(MESSAGE_EXPENSES_LISTED_OVERVIEW, 0);
         TitleContainsKeywordsPredicate predicate = preparePredicate(" ");
         FindCommandStub superCommand = new FindCommandStub(predicate);
-        FindTransactionCommand findTransactionCommand = new FindTransactionCommand(superCommand);
-        expectedModel.updateFilteredTransactionList(predicate);
-        assertCommandSuccess(findTransactionCommand, model, expectedMessage, expectedModel);
-        assertEquals(Collections.emptyList(), model.getFilteredTransactionList());
+        FindExpenseCommand findExpenseCommand = new FindExpenseCommand(superCommand);
+        expectedModel.updateFilteredExpenseList(predicate);
+        assertCommandSuccess(findExpenseCommand, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredExpenseList());
     }
 
     @Test
-    public void execute_multipleKeywords_multipleTransactionsFound() {
-        String expectedMessage = String.format(MESSAGE_TRANSACTIONS_LISTED_OVERVIEW, 3);
+    public void execute_multipleKeywords_multipleExpensesFound() {
+        String expectedMessage = String.format(MESSAGE_EXPENSES_LISTED_OVERVIEW, 3);
         TitleContainsKeywordsPredicate predicate = preparePredicate("Kurz Elle Kunz");
         FindCommandStub superCommand = new FindCommandStub(predicate);
-        FindTransactionCommand findTransactionCommand = new FindTransactionCommand(superCommand);
-        expectedModel.updateFilteredTransactionList(predicate);
-        assertCommandSuccess(findTransactionCommand, model, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredTransactionList());
+        FindExpenseCommand findExpenseCommand = new FindExpenseCommand(superCommand);
+        expectedModel.updateFilteredExpenseList(predicate);
+        assertCommandSuccess(findExpenseCommand, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(CARL, ELLE, FIONA), model.getFilteredExpenseList());
     }
 
     /**
