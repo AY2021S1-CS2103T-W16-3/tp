72133102fef19c70eaeb1030a51fb8709c02f5ae
Ian Yong <ianyongyc@gmail.com>
2020-10-18 16:32:11 +0800

    Update TypicalTransactions
    Use more realistic transactions instead of persons

--- src/test/java/ay2021s1_cs2103_w16_3/finesse/model/transaction/TransactionTest.java
+++ src/test/java/ay2021s1_cs2103_w16_3/finesse/model/transaction/TransactionTest.java
@@ -1,12 +1,12 @@
 package ay2021s1_cs2103_w16_3.finesse.model.transaction;
 
-import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_AMOUNT_BOB;
-import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_CATEGORY_HUSBAND;
-import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_DATE_BOB;
-import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_TITLE_BOB;
+import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_AMOUNT_INTERNSHIP;
+import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_CATEGORY_WORK;
+import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_DATE_INTERNSHIP;
+import static ay2021s1_cs2103_w16_3.finesse.logic.commands.CommandTestUtil.VALID_TITLE_INTERNSHIP;
 import static ay2021s1_cs2103_w16_3.finesse.testutil.Assert.assertThrows;
-import static ay2021s1_cs2103_w16_3.finesse.testutil.TypicalTransactions.ALICE;
-import static ay2021s1_cs2103_w16_3.finesse.testutil.TypicalTransactions.BOB;
+import static ay2021s1_cs2103_w16_3.finesse.testutil.TypicalTransactions.BUBBLE_TEA;
+import static ay2021s1_cs2103_w16_3.finesse.testutil.TypicalTransactions.INTERNSHIP_2;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -22,38 +22,43 @@ public class TransactionTest {
         assertThrows(UnsupportedOperationException.class, () -> transaction.getCategories().remove(0));
     }
 
+    // TODO: Not properly fixing this since this will be deleted once {@code Transaction} is made abstract.
     @Test
     public void equals() {
-        // same values -> returns true
-        Transaction aliceCopy = new TransactionBuilder(ALICE).build();
-        assertTrue(ALICE.equals(aliceCopy));
+        // same values (expense) -> returns true
+        Expense bubbleTeaCopy = new TransactionBuilder(BUBBLE_TEA).buildExpense();
+        assertTrue(BUBBLE_TEA.equals(bubbleTeaCopy));
+
+        // same values (income) -> returns true
+        Income internshipCopy = new TransactionBuilder(INTERNSHIP).buildIncome();
+        assertTrue(INTERNSHIP.equals(internshipCopy));
 
         // same object -> returns true
-        assertTrue(ALICE.equals(ALICE));
+        assertTrue(BUBBLE_TEA.equals(BUBBLE_TEA));
 
         // null -> returns false
-        assertFalse(ALICE.equals(null));
+        assertFalse(BUBBLE_TEA.equals(null));
 
         // different type -> returns false
-        assertFalse(ALICE.equals(5));
+        assertFalse(BUBBLE_TEA.equals(5));
 
         // different transaction -> returns false
-        assertFalse(ALICE.equals(BOB));
+        assertFalse(BUBBLE_TEA.equals(INTERNSHIP_2));
 
         // different title -> returns false
-        Transaction editedAlice = new TransactionBuilder(ALICE).withTitle(VALID_TITLE_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        Transaction editedBubbleTea = new TransactionBuilder(BUBBLE_TEA).withTitle(VALID_TITLE_INTERNSHIP).build();
+        assertFalse(BUBBLE_TEA.equals(editedBubbleTea));
 
         // different amounts -> returns false
-        editedAlice = new TransactionBuilder(ALICE).withAmount(VALID_AMOUNT_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        editedBubbleTea = new TransactionBuilder(BUBBLE_TEA).withAmount(VALID_AMOUNT_INTERNSHIP).build();
+        assertFalse(BUBBLE_TEA.equals(editedBubbleTea));
 
         // different dates -> returns false
-        editedAlice = new TransactionBuilder(ALICE).withDate(VALID_DATE_BOB).build();
-        assertFalse(ALICE.equals(editedAlice));
+        editedBubbleTea = new TransactionBuilder(BUBBLE_TEA).withDate(VALID_DATE_INTERNSHIP).build();
+        assertFalse(BUBBLE_TEA.equals(editedBubbleTea));
 
         // different categories -> returns false
-        editedAlice = new TransactionBuilder(ALICE).withCategories(VALID_CATEGORY_HUSBAND).build();
-        assertFalse(ALICE.equals(editedAlice));
+        editedBubbleTea = new TransactionBuilder(BUBBLE_TEA).withCategories(VALID_CATEGORY_WORK).build();
+        assertFalse(BUBBLE_TEA.equals(editedBubbleTea));
     }
 }
