c73f1df1240d3aef60b0683f322aaf4723daeab2
yongping827 <yongping827@users.noreply.github.com>
2020-10-24 17:56:11 +0800

    Add dummy bar chart

--- src/main/java/ay2021s1_cs2103_w16_3/finesse/ui/tabs/AnalyticsTabPane.java
+++ src/main/java/ay2021s1_cs2103_w16_3/finesse/ui/tabs/AnalyticsTabPane.java
@@ -2,7 +2,13 @@ package ay2021s1_cs2103_w16_3.finesse.ui.tabs;
 
 import ay2021s1_cs2103_w16_3.finesse.model.budget.MonthlyBudget;
 import ay2021s1_cs2103_w16_3.finesse.ui.UiPart;
+import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.StackPane;
 
 /**
  * Tab pane that displays analytics.
@@ -10,12 +16,32 @@ import javafx.scene.canvas.Canvas;
 public class AnalyticsTabPane extends UiPart<Canvas> {
     private static final String FXML = "AnalyticsTabPane.fxml";
 
+    @FXML
+    private StackPane pane;
+
+    @FXML
+    private BarChart<String,Number> analytics;
+
     /**
      * Creates an {@code AnalyticsTabPane}.
      */
     public AnalyticsTabPane(MonthlyBudget monthlyBudget) {
         super(FXML);
+
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        analytics = new BarChart<>(xAxis, yAxis);
+
+        XYChart.Series<String,Number> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("cat", 1));
+
+        analytics.getData().add(series);
+
+        pane.getChildren().add(analytics);
     }
 
     // TODO: Add visualizations.
+
+
 }
