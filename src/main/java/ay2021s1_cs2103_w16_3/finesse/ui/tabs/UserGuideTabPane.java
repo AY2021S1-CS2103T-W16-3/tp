e7f2b99898741481951d5fef62af421b9594e74a
Ian Yong <ianyongyc@gmail.com>
2020-10-27 00:16:37 +0800

    Add URL whitelisting

--- src/main/java/ay2021s1_cs2103_w16_3/finesse/ui/tabs/UserGuideTabPane.java
+++ src/main/java/ay2021s1_cs2103_w16_3/finesse/ui/tabs/UserGuideTabPane.java
@@ -1,8 +1,10 @@
 package ay2021s1_cs2103_w16_3.finesse.ui.tabs;
 
 import ay2021s1_cs2103_w16_3.finesse.ui.UiPart;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 /**
@@ -10,7 +12,10 @@ import javafx.scene.web.WebView;
  */
 public class UserGuideTabPane extends UiPart<StackPane> {
     private static final String FXML = "UserGuideTabPane.fxml";
+
+    private static final String GITHUB_PAGES_DOMAIN = "https://ay2021s1-cs2103t-w16-3.github.io";
     private static final String USER_GUIDE_URL = "https://ay2021s1-cs2103t-w16-3.github.io/tp/UserGuide.html";
+    private static final String ERROR_PAGE_URL = "https://ay2021s1-cs2103t-w16-3.github.io";
 
     @FXML
     private WebView webView;
@@ -20,10 +25,25 @@ public class UserGuideTabPane extends UiPart<StackPane> {
      */
     public UserGuideTabPane() {
         super(FXML);
-        initialiseUserGuide();
+        initializeUserGuide();
     }
 
-    private void initialiseUserGuide() {
-        webView.getEngine().load(USER_GUIDE_URL);
+    /**
+     * Initializes the {@code WebEngine} that is backing the {@code WebView}.
+     */
+    private void initializeUserGuide() {
+        WebEngine webEngine = webView.getEngine();
+
+        webEngine.locationProperty().addListener((observableValue, oldUrl, newUrl) -> {
+            if (!newUrl.contains(GITHUB_PAGES_DOMAIN)) {
+                // Block requests to external sites.
+                Platform.runLater(() -> {
+                    // Load the error page.
+                    webEngine.load(ERROR_PAGE_URL);
+                });
+            }
+        });
+
+        webEngine.load(USER_GUIDE_URL);
     }
 }
